e16c2acd2bc55944e92b5846b8191b7f510a1211
Andrew Rabert <ar@nullsum.net>
2018-12-03 17:13:17 -0500

    Extend raven

--- sprockets/mixins/sentry/__init__.py
+++ sprockets/mixins/sentry/__init__.py
@@ -4,7 +4,6 @@ mixins.sentry
 A RequestHandler mixin for sending exceptions to Sentry
 
 """
-import logging
 import math
 import os
 import re
@@ -13,9 +12,6 @@ import time
 from raven.processors import SanitizePasswordsProcessor
 import raven
 import raven.contrib.tornado
-
-LOGGER = logging.getLogger(__name__)
-SENTRY_CLIENT = 'sentry_client'
 
 version_info = (1, 2, 0)
 __version__ = '.'.join(str(v) for v in version_info)
@@ -29,8 +25,6 @@ URI_RE = re.compile(r"^[\w\+\-]+://"
                     r"([^@]+)"
                     r"@",
                     re.IGNORECASE)
-
-_sentry_warning_issued = False
 
 
 class SanitizeEmailsProcessor(SanitizePasswordsProcessor):
@@ -83,31 +77,7 @@ class SentryMixin(raven.contrib.tornado.SentryMixin):
 
     Mix this in over a :class:`tornado.web.RequestHandler` to report
     unhandled exceptions to Sentry so that you can figure out what
-    went wrong.  In order to use this mix-in, all that you have to do
-    is define the **SENTRY_DSN** environment variable that contains your
-    projects Sentry DSN.  Whenever a request comes it and the environment
-    variable is set, this mix-in will create a new :class:`raven.base.Client`
-    instance and make it available via the :attr:`sentry_client` property.
-
-    If an exception is caught by :meth:`._handle_request_exception`, then
-    it will be reported to Sentry in all it's glory.
-
-    .. attribute:: sentry_client
-
-       The :class:`raven.base.Client` instance or :data:`None` if sentry
-       reporting is disabled.  You can modify attributes of the client
-       as required for your application -- for example, you can add new
-       modules by adding to ``self.sentry_client.include_paths``.
-
-    .. attribute:: sentry_extra
-
-       A :class:`dict` of extra information to pass to sentry when an
-       exception is reported.
-
-    .. attribute:: sentry_tags
-
-       A :class:`dict` of tag and value pairs to associated with any
-       reported exceptions.
+    went wrong.
 
     """
 
@@ -129,67 +99,3 @@ class SentryMixin(raven.contrib.tornado.SentryMixin):
             if matches:
                 values[key] = values[key].replace(matches.group(1), '****')
         return values
-
-
-def install(application, **kwargs):
-    """
-    Call this to install a sentry client into a Tornado application.
-
-    :param tornado.web.Application application: the application to
-        install the client into.
-    :param kwargs: keyword parameters to pass to the
-        :class:`raven.base.Client` initializer.
-
-    :returns: :data:`True` if the client was installed by this call
-        and :data:`False` otherwise.
-
-    This function should be called to initialize the Sentry client
-    for your application.  It will be called automatically with the
-    default parameters by :class:`.SentryMixin` if you do not call
-    it during the creation of your application.  You should install
-    the client explicitly so that you can set at least the following
-    properties:
-
-    - **include_paths** list of python modules to include in tracebacks.
-      This function ensures that ``raven``, ``sprockets``, ``sys``, and
-      ``tornado`` are included but you probably want to include additional
-      packages.
-
-    - **release** the version of the application that is running
-
-    See `the raven documentation`_ for additional information.
-
-    .. _the raven documentation: https://docs.getsentry.com/hosted/clients/
-       python/advanced/#client-arguments
-
-    """
-    sentry_dsn = kwargs.pop('dsn', os.environ.get('SENTRY_DSN'))
-    if sentry_dsn is None:
-        global _sentry_warning_issued
-        if not _sentry_warning_issued:
-            LOGGER.info('sentry DSN not found, not installing client')
-            _sentry_warning_issued = True
-        setattr(application, 'sentry_client', None)
-        return False
-
-    # ``include_paths`` has two purposes:
-    # 1. it tells sentry which parts of the stack trace are considered
-    #    part of the application for use in the UI
-    # 2. it controls which modules are included in the version dump
-    include_paths = set(kwargs.pop('include_paths', []))
-    include_paths.update(['raven', 'sprockets', 'sys', 'tornado', __name__])
-    kwargs['include_paths'] = list(include_paths)
-
-    # ``exclude_paths`` tells the sentry UI which modules to exclude
-    # from the "In App" view of the traceback.
-    exclude_paths = set(kwargs.pop('exclude_paths', []))
-    exclude_paths.update(['raven', 'sys', 'tornado'])
-    kwargs['exclude_paths'] = list(exclude_paths)
-
-    if os.environ.get('ENVIRONMENT'):
-        kwargs.setdefault('environment', os.environ['ENVIRONMENT'])
-
-    client = raven.Client(sentry_dsn, **kwargs)
-    setattr(application, 'sentry_client', client)
-
-    return True
